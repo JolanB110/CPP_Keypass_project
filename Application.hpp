beb9f5344630de560594ab43aa8564f55fa1fd2c
JolanB110 <jolan110.buisset@gmail.com>
2025-12-09 14:29:40 +0100

    added application constructors

--- Application.hpp
+++ Application.hpp
@@ -8,8 +8,14 @@ class Application {
 public:
     std::string name;      
     std::string type;
-
+    
 private:
+    //Constructeurs
+    Application(const std::string& name, const std::string& type);
+    Application(const std::string& name, const std::string& type, std::string mail);
+    Application(const std::string& name, const std::string& type, const std::string mail, const std::string path_to_user);
+    Application(const std::string& name, const std::string& type, const std::string mail, const std::string path_to_user, int numero_carte, int CCV, const std::string date_expiration);
+
     std::string mail;
     std::string path_to_user;
     int numero_carte;
