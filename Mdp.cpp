07f0aa2b7c49a38ecd77a60191e0b3b3da25c38f
JolanB110 <jolan110.buisset@gmail.com>
2025-12-16 15:09:49 +0100

    Balenced Mdp.hpp and Mdp.cpp

--- Mdp.cpp
+++ Mdp.cpp
@@ -1,2 +1,34 @@
 #include "Mdp.hpp"
 
+// Constructeurs
+Mdp::Mdp(const std::string& name, const std::string& Password)
+    : name(name), Password(Password) {}
+
+Mdp::Mdp(const std::string& name, const std::string& Password, const std::string& label)
+    : name(name), Password(Password), label(label) {}
+
+// Getters
+std::string Mdp::getName() const { return name; }
+std::string Mdp::getPassword() const { return Password; }
+std::string Mdp::getLabel() const { return label; }
+
+// Générateur de mot de passe
+std::string Mdp::mdpgenerator(int length, bool includeSpecial) {
+    std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+    std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string numbers = "0123456789";
+    std::string special = "!@#$%^&*-_+=";
+
+    std::string charset = lowercase + uppercase + numbers;
+    if (includeSpecial) charset += special;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, charset.length() - 1);
+
+    std::string password;
+    for (int i = 0; i < length; i++) {
+        password += charset[dis(gen)];
+    }
+    return password;
+}
--- Mdp.hpp
+++ Mdp.hpp
@@ -3,54 +3,21 @@
 
 #include <string>
 #include <random>
-#include <algorithm>
 
 class Mdp {
-
 public:
-    std::string name;      
+    std::string name;
     std::string Password;
     std::string label;
 
-    Mdp(const std::string& name, const std::string& Password)
-        : name(name), Password(Password) {}
-    
-    Mdp(const std::string& name, const std::string& Password, const std::string& label)
-        : name(name), Password(Password), label(label) {}
+    Mdp(const std::string& name, const std::string& Password);
+    Mdp(const std::string& name, const std::string& Password, const std::string& label);
 
-    std::string getName() const { 
-        return name; 
-    }
+    std::string getName() const;
+    std::string getPassword() const;
+    std::string getLabel() const;
 
-    std::string getPassword() const { 
-        return Password; 
-    }
-
-    std::string getLabel() const {
-        return label;
-    }
-
-    static std::string mdpgenerator(int length = 16, bool includeSpecial = true) {
-        std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
-        std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        std::string numbers = "0123456789";
-        std::string special = "!@#$%^&*-_+=";
-    
-        std::string charset = lowercase + uppercase + numbers;
-        if (includeSpecial) {
-            charset += special;
-            }
-    
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(0, charset.length() - 1);
-    
-        std::string password;
-        for (int i = 0; i < length; i++) {
-            password += charset[dis(gen)];
-        }
-        return password;
-}
+    static std::string mdpgenerator(int length = 16, bool includeSpecial = true);
 };
 
 #endif
